2ada605817142e46fd9fb32013400c6e42974a0c
Aline Alves De Souza <alialves@c1r3p1.42sp.org.br>
2023-11-05 20:23:42 -0300

    mudando algo

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,40 +6,30 @@
 /*   By: alialves <alialves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 19:10:42 by alialves          #+#    #+#             */
-/*   Updated: 2023/11/05 19:21:40 by alialves         ###   ########.fr       */
+/*   Updated: 2023/11/05 20:17:50 by alialves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_check_char(char c, char const *set)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	while (*set)
+	int		i;
+	char	*new_str;
+	int		len;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	new_str = (char *)malloc(len + 1);
+	if (!new_str)
+		return (NULL);
+	new_str[len] = '\0';
+	i = 0;
+	while (i < len)
 	{
-		if (*set == c)
-			return (1);
-		set++;
+		new_str[i] = (*f)(i, s[i]);
+		i++;
 	}
-	return (0);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t		start;
-	size_t		end;
-	char		*str;
-
-	if (!set || !s1)
-		return (NULL);
-	start = 0;
-	while (s1[start] && ft_check_char(s1[start], set))
-		start++;
-	end = ft_strlen(s1);
-	while (end > start && ft_check_char(s1[end - 1], set))
-		end--;
-	str = (char *)malloc((end - start + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	ft_strlcpy(str, (s1 + start), end - start + 1);
-	return (str);
+	return (new_str);
 }
